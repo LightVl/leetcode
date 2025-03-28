9285a9202d44d5ef365a0a13a64120d22c70fdf5
LightVl <30297106+LightVl@users.noreply.github.com>
2025-03-28 15:54:39 +0300

    Time: 16 ms (16.57%), Space: 44.9 MB (40.88%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,18 +2,7 @@ class Solution {
     public boolean isPalindrome(String s) {
         s = s.replaceAll("[^a-zA-Z0-9]", "");
         s = s.toLowerCase();
-        System.out.println(s);
-        int i = 0;
-        int j = s.length()-1;
-        while (i!=s.length()/2) {
-            if (s.charAt(i)!=s.charAt(j)) {
-                System.out.println(false);
-                return false;
-            }
-            i++;
-            j--;
-        }
-        System.out.println(true);
-        return true;
+        String s2 = new StringBuilder(s).reverse().toString();
+        return s.equals(s2);
     }
 }
