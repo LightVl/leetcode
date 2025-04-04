172968c4a765077a53ae320263b0dabfab46d576
LightVl <30297106+LightVl@users.noreply.github.com>
2025-04-04 19:43:41 +0300

    Time: 25 ms (5.32%), Space: 47.3 MB (6.79%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -3,32 +3,23 @@ class Solution {
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         System.out.println(Arrays.deepToString(intervals));
         List<int[]> listResult = new ArrayList<int[]>();
-        int firstNum = intervals[0][0];
-        int secondNum = intervals[0][1];
-        int i = 0;
-        while (true) {
-            if (i==intervals.length-1) {
-                secondNum = (secondNum > intervals[i][1]) ? secondNum : intervals[i][1];
-                listResult.add(new int[]{firstNum,secondNum});
-                break;
-            }
-            while (secondNum >= intervals[i + 1][0]) {
+        int firstNum=intervals[0][0];
+        int secondNum=intervals[0][1];
+        int i;
+        finish:
+        for(i=0;i<intervals.length-1;i++) {
+            while (secondNum >= intervals[i + 1][0] && i<intervals.length-1) {
                 secondNum = (secondNum > intervals[i + 1][1]) ? secondNum : intervals[i + 1][1];
                 i++;
-                if (i==intervals.length-1) {
-                    secondNum = (secondNum > intervals[i][1]) ? secondNum : intervals[i][1];
-                    listResult.add(new int[]{firstNum,secondNum});
-                    break;
-                }
-            }
-            if (i==intervals.length-1) {
-                break;
+                if (i== intervals.length-1)
+                    break finish;
             }
             listResult.add(new int[]{firstNum,secondNum});
             firstNum=intervals[i+1][0];
             secondNum=intervals[i+1][1];
-            i++;
         }
+        secondNum = (secondNum > intervals[i][1]) ? secondNum : intervals[i][1];
+        listResult.add(new int[]{firstNum,secondNum});
         int[][] result = new int[listResult.size()][2];
         for (i = 0; i < listResult.size(); i++) {
             result[i] = listResult.get(i);
