d1ebd1aab6fe199c9ec2367fbf65a78e3071a6d5
LightVl <30297106+LightVl@users.noreply.github.com>
2025-04-05 13:18:58 +0300

    Time: 14 ms (5.13%), Space: 44.6 MB (16.4%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String oneSlashPath = path.replaceAll("/+","/");
+        Stack<String> stack = new Stack<>();
+        System.out.println(oneSlashPath);
+        String[] splitted = oneSlashPath.split("/",0);
+        System.out.println(Arrays.toString(splitted));
+        StringBuilder result = new StringBuilder();
+        for (String c : splitted) {
+            switch (c) {
+                case "":
+                case ".": break;
+                case"..":
+                    if(stack.empty()) {
+                    break;
+                } else
+                    stack.pop();
+                    break;
+                default:
+                    stack.push(c);
+            }
+        }
+        if(stack.empty()) return "/";
+        String[] arr = stack.toArray(new String[stack.size()]);
+        for (String a:arr) {
+            result.append("/");
+            result.append(a);
+        }
+        System.out.println(result.toString());
+        return result.toString();  
+    }
+}
