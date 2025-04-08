99ede5a1a08e87a37d80f1bb8ddad8c72d8e3495
LightVl <30297106+LightVl@users.noreply.github.com>
2025-04-08 20:08:23 +0300

    Time: 7 ms (6.1%), Space: 41.8 MB (65.97%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,39 +1,38 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
+        //Character[] data = { '(', '[', '{', ')', ']', '}' };
+        ArrayList<Character> dataList = new ArrayList<>(Arrays.asList('(', '[', '{', ')', ']', '}'));
+        //dataList.addAll(Arrays.asList(data));
+        System.out.println(dataList.get(0));
+        System.out.println(dataList.get(3));
         for (Character c : s.toCharArray()) {
-            switch (c) {
-                case '(':
-                case '[':
-                case '{':
-                    stack.push(c);
-                    break;
-                case ')':
-                    if (!stack.isEmpty()&&stack.peek() == '(') {
-                        stack.pop();
-                    } else {
-                        return false;
-                    }
-                    break;
-                case ']':
-                    if (!stack.isEmpty()&&stack.peek() == '[') {
-                        stack.pop();
-                    } else {
-                        return false;
-                    }
-                    break;
-                case '}':
-                    if (!stack.isEmpty()&&stack.peek() == '{') {
-                        stack.pop();
-                    } else {
-                        return false;
-                    }
-                    break;
-                default:
-                    System.out.println(false);
-                    return false;
+            if (c == dataList.get(0) || c == dataList.get(1) || c == dataList.get(2)) {
+                stack.push(c);
             }
+            if (c == dataList.get(3)) {
+                if (!stack.isEmpty() && stack.peek() == dataList.get(0)) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+            if (c == dataList.get(4)) {
+                if (!stack.isEmpty() && stack.peek() == dataList.get(1)) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+            if (c == dataList.get(5)) {
+                if (!stack.isEmpty() && stack.peek() == dataList.get(2)) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+
         }
-        return stack.isEmpty();  
+        return stack.isEmpty();
     }
 }
