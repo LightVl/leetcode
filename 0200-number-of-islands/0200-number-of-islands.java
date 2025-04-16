899c51a375511abf4d2099e37ef0f30acb798612
LightVl <30297106+LightVl@users.noreply.github.com>
2025-04-16 20:32:02 +0300

    Time: 4 ms (49.6%), Space: 52.4 MB (14.98%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int islands = 0;
+        List<coord> island = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        //ищем острова
+        for (i = 0; i < m; i++) {
+            for (j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    islands++;
+                    searchland(i, j, grid);
+                }
+            }
+        }
+        return islands;
+    }
+
+    public static void searchland(int x, int y, char[][] grid) {
+        coord[] directions = new coord[4];
+        directions[0] = new coord(-1, 0);
+        directions[1] = new coord(0, -1);
+        directions[2] = new coord(1, 0);
+        directions[3] = new coord(0, 1);
+        grid[x][y] = '0';
+        for (int k = 0; k < 4; k++) {
+            int i = x + directions[k].x;
+            int j = y + directions[k].y;
+            if (isinside(i, j, grid)) {
+                if (grid[i][j] == '1') {
+                    searchland(i, j, grid);
+                }
+            }
+        }
+
+    }
+
+    public static boolean isinside(int x, int y, char[][] grid) {
+        if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length) {
+            return false;
+        } else
+            return true;
+    }
+
+    record coord(int x, int y) {
+
+    }
+}
