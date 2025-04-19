42e32c8091a75817a2ecf1c3a4062beb22b032fe
LightVl <30297106+LightVl@users.noreply.github.com>
2025-04-19 13:02:22 +0300

    Time: 11 ms (9.57%), Space: 42 MB (19.53%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
@@ -8,34 +8,24 @@ class Solution {
         mutationMap.put('G', "ATC");
         Deque<res> queue = new ArrayDeque<>();
         queue.offer(new res(startGene,0));
-        System.out.println(queue.peek().gene);
         while (true)
             next:{
             if (queue.isEmpty())
                 return -1;
             if(queue.peek().gene.equals(endGene)) {
-                System.out.println(queue.peek().i);
-                //return;
                 return queue.peek().i;
             }
-            /*if (mut.isEmpty()) {
-                System.out.println(-1);
-                return -1;
-            }*/
             for(int i=0;i<queue.peek().gene.length();i++) {
                 for(char b: mutationMap.get(queue.peek().gene.charAt(i)).toCharArray()) {
                     String newGene = queue.peek().gene.substring(0,i)+b+queue.peek().gene.substring(i+1);
-                    //System.out.println(newGene);
                     if (mut.contains(newGene)) {
                         System.out.println("===");
-                        System.out.println(newGene);
                         queue.offerFirst(new res(newGene,queue.peek().i+1));
                         mut.remove(newGene);
                         break next;
                     }
                 }
             }
-            //return -1;
             queue.pop();
         }
     }
